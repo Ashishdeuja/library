d745c8df730c4c41d3e0ef9dec5011cd1fe3c25d
Ashish Deuja <86704158+Ashishdeuja@users.noreply.github.com>
2022-08-18 14:30:46 +0545

    API added for book

--- library_project/settings.py
+++ library_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4a=71)yni(#5bzr-*j&ceb8+_wt1*z&gdwqu1ljlp%1d(q(v!m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["library.herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -36,17 +36,19 @@ Django_Apps = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
 ]
 
 
 Project_Apps = [
-    'books'
+    'books',
+    'apis'
 ]
 
 
 Third_Party_Apps = [
-
+    'rest_framework'
 ]
 
 
@@ -56,6 +58,7 @@ INSTALLED_APPS = Django_Apps + Project_Apps + Third_Party_Apps
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -129,7 +132,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"] 
+STATIC_ROOT = BASE_DIR / "staticfiles" 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
