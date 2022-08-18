1930de28603fba966153a6768740a5ed3b25ede9
Ashish Deuja <86704158+Ashishdeuja@users.noreply.github.com>
2022-08-18 15:03:21 +0545

    some changes

--- library_project/settings.py
+++ library_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4a=71)yni(#5bzr-*j&ceb8+_wt1*z&gdwqu1ljlp%1d(q(v!m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["demo-library.herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["demo22-library.herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
